1d875e1be0e1d810dc9219ea5b207b60cc32b87b
Erich Wellinger <ewellinger@icloud.com>
2015-06-05 00:40:29 -0600

    Worked through Ch01

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -13,12 +13,60 @@ import sys
 import nsfg
 import thinkstats2
 
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz',
+                nrows=None):
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    CleanFemResp(df)
+    return df
+
+def CleanFemResp(df):
+    """Recodes variables from the respondent frame.
+
+    df: DataFrame
+    """
+    pass
+
+def ValidatePregnum(resp):
+    """Validate pregnum in the respondent file.
+
+    resp: respondent DataFrame
+    """
+    # Read the pregnancy frame
+    preg = nsfg.ReadFemPreg()
+
+    # Make a dictionary that maps caseid to list of indices
+    preg_map = nsfg.MakePregMap(preg)
+
+    # iterate through the respondent pregnum series
+    for index, pregnum in resp.pregnum.iteritems():
+        caseid = resp.caseid[index]
+        indicies = preg_map[caseid]
+
+        # check that pregnum from the respondent file equals
+        # the number of records in the pregnancy file
+        if len(indicies) != pregnum:
+            print(caseid, len(indicies), pregnum)
+            return False
+
+    return True
+
+
+
 
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
+    resp = ReadFemResp()
+
+    assert(len(resp) == 7643)
+    assert(resp.pregnum.value_counts()[1] == 1267)
+    assert(ValidatePregnum(resp))
+
+
     print('%s: All tests passed.' % script)
 
 
